2fbe2c201f6f21935f9e9bbe11a9cf1c5f4c542b
Eric <ysj19921026@163.com>
2018-02-01 17:34:38 +0800

    add a change img function

--- first-site/scripts/main.js
+++ first-site/scripts/main.js
@@ -1,2 +1,12 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent='Hello World!';
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent='Hello World!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/moon-over-the-mountain-landscape.jpg') {
+        myImage.setAttribute('src', 'images/mountains-with-lake-majestic-landscape.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/moon-over-the-mountain-landscape.jpg');
+    }
+}
