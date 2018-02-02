f019b794105f551aba8868682ef994fa007cf508
Eric <ysj19921026@163.com>
2018-02-02 14:45:20 +0800

    add welcome message with name changing button

--- first-site/scripts/main.js
+++ first-site/scripts/main.js
@@ -10,3 +10,23 @@ myImage.onclick = function () {
         myImage.setAttribute('src', 'images/moon-over-the-mountain-landscape.jpg');
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'WebDev is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'WevDev is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
